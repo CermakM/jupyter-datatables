b014921a9a7a95858cbe43f0f4fe65c12e675641
Marek Cermak <macermak@redhat.com>
2019-04-11 10:05:59 +0200

    Setuptools
    Signed-off-by: Marek Cermak <macermak@redhat.com>
    
    new file:   jupyter_datatables/__about__.py
    new file:   Pipfile
    new file:   requirements.in
    new file:   requirements.txt
    new file:   setup.cfg
    new file:   setup.py

---
+++ jupyter_datatables/__about__.py
@@ -0,0 +1,18 @@
+"""About this package."""
+
+__all__ = [
+    "__title__", "__summary__", "__uri__", "__version__", "__author__",
+    "__email__", "__license__", "__copyright__",
+]
+
+__title__ = "jupyter-datatables"
+__summary__ = "Jupyter Notebook extension to levarage pandas DataFrames by integrating DataTables JS."
+__uri__ = "https://github.com/CermakM/jupyter-datatables"
+
+__version__ = "0.1.0"
+
+__author__ = "Marek Cermak"
+__email__ = "macermak@redhat.com"
+
+__license__ = "MIT"
+__copyright__ = "Copyright 2019 {0}".format(__author__)
---
+++ setup.py
@@ -0,0 +1,54 @@
+# -*- coding: utf-8 -*-
+
+from __future__ import print_function
+
+from pathlib import Path
+from setuptools import setup
+from setuptools import find_packages
+
+
+HERE = Path(__file__).parent
+NAME = 'jupyter_datatables'
+
+ABOUT = dict()
+exec(Path(HERE, NAME, '__about__.py').read_text(), ABOUT)
+
+README: str = Path(HERE, "README.md").read_text(encoding='utf-8')
+REQUIREMENTS: list = Path(HERE, 'requirements.txt').read_text().splitlines()
+
+setup(
+    name=ABOUT['__title__'],
+    version=ABOUT['__version__'],
+
+    author=ABOUT['__author__'],
+    author_email=ABOUT['__email__'],
+    url=ABOUT['__uri__'],
+
+    license=ABOUT['__license__'],
+
+    description=ABOUT['__summary__'],
+    long_description=README,
+    long_description_content_type='text/markdown',
+
+    classifiers=[
+        "Development Status :: 2 - Pre-Alpha",
+        "Framework :: IPython",
+        "Framework :: Jupyter",
+        "License :: OSI Approved :: MIT License",
+        "Natural Language :: English",
+        "Operating System :: OS Independent",
+        "Programming Language :: JavaScript",
+        "Programming Language :: Python :: 3.5",
+        "Programming Language :: Python :: 3.6",
+        "Programming Language :: Python :: 3.7",
+        "Topic :: Scientific/Engineering :: Visualization",
+        "Topic :: Utilities"
+    ],
+
+    install_requires=REQUIREMENTS,
+
+    packages=find_packages(),
+    include_package_data=True,
+
+    zip_safe=False
+)
