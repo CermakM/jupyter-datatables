23d3309970d6d044d9c3bf4255ba83153db6b24d
Marek Cermak <macermak@redhat.com>
2019-04-15 11:08:10 +0200

    Histogram preview
    Signed-off-by: Marek Cermak <macermak@redhat.com>
    
    modified:   jupyter_datatables/js/jupyter-datatables.js

--- jupyter_datatables/js/jupyter-datatables.js
+++ jupyter_datatables/js/jupyter-datatables.js
@@ -2,58 +2,74 @@ define('jupyter-datatables', function (require) {
     let DT = require("datatables.net");
     let d3 = require("d3");
 
-    let plot = function (data, container, margin) {
-            const width  = 600,
-                  height = 400;
+    let _hist_bin_fd = function(a) {
+        return 2 * (d3.quantile(a, .75) - d3.quantile(a, .25)) * Math.pow(a.length, -1 / 3);
+    };
+
+    let _hist_bin_sturges = function(a) {
+        return ( a[a.length - 1] - a[0] ) / ( Math.log2(a.length) + 1);
+    };
+
+    let _hist_bin_auto = function(a) {
+        const bin_width_fd = _hist_bin_fd(a),
+              bin_width_sturges = _hist_bin_sturges(a);
             
-            // TODO: Implement Freedman-Diaconis rule
-            const n_bins = 10;
+        return bin_width_fd ? Math.min(bin_width_fd, bin_width_sturges) : bin_width_sturges;
+    }
 
-            margin = {
-                left: 5,
-                right: 5,
-                top: 10,
-                bottom: 10
-            };
+    let plot = function (data, container, margin) {
+        data = Array.prototype.map.call(data, Number).sort(d3.ascending);
 
-            let svg_container = d3.select(container)
-                .append('div')
-                .classed('svg-container', true);
+        margin = {
+            left: 5,
+            right: 5,
+            top: 10,
+            bottom: 10
+        };
 
-            let svg = svg_container.append('svg')
-                .attr('preserveAspectRatio', 'xMinYMin meet')
-                .attr('viewBox', `0 0 ${width} ${height}`)
-                .classed('svg-content', true);
+        const width  = 600,
+              height = 400;
+        
+        const n_bins = Math.ceil(( data[data.length - 1] - data[0] ) / _hist_bin_auto(data));
 
-            let g = svg
-                .append('g')
-                .classed('bars', true);
+        let svg_container = d3.select(container instanceof jQuery ? container.get(0) : container)
+            .append('div')
+            .classed('svg-container', true);
 
-            let x_range = d3.scaleLinear()
-                .domain(d3.extent(data))
-                .nice()
-                .range([0, width]);
+        let svg = svg_container.append('svg')
+            .attr('preserveAspectRatio', 'xMinYMin meet')
+            .attr('viewBox', `0 0 ${width} ${height}`)
+            .classed('svg-content', true);
 
-            let bins = d3.histogram()
-                .domain(x_range.domain())
-                .thresholds(x_range.ticks(n_bins))
-                (data);
+        let g = svg
+            .append('g')
+            .classed('bars', true);
 
-            let y_range = d3.scaleLinear()
-                .domain([0, d3.max(bins, d => d.length)])
-                .nice()
-                .range([height, 0]); // reverse the domain
+        let x_range = d3.scaleLinear()
+            .domain(d3.extent(data))
+            .nice()
+            .range([0, width]);
 
-            g.selectAll('.bar')
-                .data(bins)
-                .enter()
-                .append('rect')
-                .attr('x', (d) => x_range(d.x0) + 1)
-                .attr('y', (d) => y_range(d.length))
-                .attr('width', (d) => Math.max(0, x_range(d.x1) - x_range(d.x0) - 1))
-                .attr('height', (d) => y_range(0) - y_range(d.length))
-                .attr('fill', 'steelblue')
-                .classed('bar', true);
+        let bins = d3.histogram()
+            .domain(x_range.domain())
+            .thresholds(x_range.ticks(n_bins))
+            (data);
+
+        let y_range = d3.scaleLinear()
+            .domain([0, d3.max(bins, d => d.length)])
+            .nice()
+            .range([height, 0]); // reverse the domain
+
+        g.selectAll('.bar')
+            .data(bins)
+            .enter()
+            .append('rect')
+            .attr('x', (d) => x_range(d.x0) + 1.5)
+            .attr('y', (d) => y_range(d.length))
+            .attr('width', (d) => Math.max(0, x_range(d.x1) - x_range(d.x0)))
+            .attr('height', (d) => y_range(0) - y_range(d.length))
+            .attr('fill', 'steelblue')
+            .classed('bar', true);
 
         return svg;
     };
