234e520b480d7611bbf3c2bcec10b867913b82cc
Marek Cermak <macermak@redhat.com>
2019-07-02 22:35:55 +0200

    Create Bar graph object using chartjs
    - add new js module `graph-objects` to define graph objects (starting
    with Bar chart)
    - refactor plotBar function into go.Bar using chartjs
    
    Signed-off-by: Marek Cermak <macermak@redhat.com>
    
    new file:   jupyter_datatables/js/graph-objects.js
    modified:   jupyter_datatables/__init__.py
    modified:   jupyter_datatables/css/jupyter-datatables.css
    modified:   jupyter_datatables/js/jupyter-datatables.js

---
+++ jupyter_datatables/js/graph-objects.js
@@ -0,0 +1,58 @@
+define("graph-objects", ["chartjs", "d3"], function (chartjs, d3) {
+
+    Chart.defaults.scale.gridLines.display = false
+
+
+    let Bar = function (data, labels) {
+        if (_.isUndefined(labels)) {
+            labels = d3.range(0, data.length)
+        }
+
+        console.debug("Bar plot data: ", data, "labels: ", labels)
+
+        let canvas = $("<canvas>")
+            .attr("width", "135px")
+            .css("margin", "auto")
+
+        canvas.ready(() => {
+
+            let ctx = canvas.get(0).getContext('2d');
+
+            barChart = new Chart(ctx, {
+                type: 'bar',
+                data: {
+                    labels: labels,
+                    datasets: [{
+                        data: data,
+                        backgroundColor: 'rgb(255, 99, 132)',
+                        borderColor: 'rgb(255, 99, 132)',
+                        borderWidth: 1
+                    }]
+                },
+                options: {
+                    legend: {
+                        display: false
+                    },
+                    scales: {
+                        xAxes: [{
+                            display: false
+                        }],
+                        yAxes: [{
+                            display: false,
+                            ticks: {
+                                beginAtZero: true
+                            }
+                        }]
+                    },
+                    responsive: false
+                }
+            });
+        })
+
+        return canvas
+    }
+
+    return {
+        Bar: Bar
+    }
+})
