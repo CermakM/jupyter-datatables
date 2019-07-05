6ff10decb0a177f3274e0ef27470857e695e0ece
Marek Cermak <macermak@redhat.com>
2019-07-05 10:53:44 +0200

    Refactor DataTables configuration
    This allows users to configure the default settings to their needs.
    
    Signed-off-by: Marek Cermak <macermak@redhat.com>
    
    modified:   jupyter_datatables/__init__.py
    modified:   jupyter_datatables/config.py

--- jupyter_datatables/config.py
+++ jupyter_datatables/config.py
@@ -8,58 +8,74 @@ from collections import namedtuple
 __all__ = ['defaults']
 
 
-Configuration = {
-    'classes': ['table', 'cell-border', 'nowrap'],
-    'options': {
-        'columnDefs': [
-            {
-                # this is the first column, assuming this is index of the table
-                'searchable': False,
-                'width': '5%',
-                'targets': 0,
-            },
-            {
-                'targets': '_all',
-                'className': 'dt-body-center dt-head-center'
-            }
-        ],
-        'buttons': [
-            'print', 'csv', 'pdf'
-        ],
+class DataTablesOptions(object):
+    "Default options for Jupyter DataTables."
 
-        'order': [],  # disable initial ordering
-        'ordering': True,
+    columnDefs = [
+        {
+            # this is the first column, assuming this is index of the table
+            'searchable': False,
+            'width': '5%',
+            'targets': 0,
+        },
+        {
+            'targets': '_all',
+            'className': 'dt-body-center dt-head-center'
+        }
+    ]
 
-        'paging': True,
+    buttons = [
+        'print', 'csv', 'pdf'
+    ]
 
-        'responsive': True,
+    order = []  # disable initial ordering
+    ordering = True
 
-        'scrollX': True,
-        'scrollY': False,
+    paging = True
 
-        'searching': True,
-    },
-    'extensions': {
-        'buttons': True,
-        'responsive': True,
-        'scroller': True,
-        'select': True
-    },
-    # Take a sample after exceeding given limit
-    #
-    # :type int: limit number of rows of the table
-    'limit': 1000,
-    'sample_size': None,
-    # Sort dataTable by index or values
-    #
-    # :type bool: enable or disable sorting by index (default: False)
-    # :type str: sort by single column
-    # :type List[str]: sort by multiple column(s)
-    'sort': False,
-    'warnings': True,
-}
+    responsive = True
 
-DefaultDataTablesConfig = namedtuple('DefaultConfig', _DEFAULT_CONFIG.keys())
+    scrollX = True
+    scrollY = False
 
-config = DefaultDataTablesConfig(**_DEFAULT_CONFIG)
-"""Default configuration for Jupyter datatables tools."""
+    searching = True
+
+class DataTablesExtensions(object):
+    """Default extensions for Jupyter DataTables."""
+
+    buttons    = True
+    responsive = True
+    scroller   = True
+    select     = True
+
+class DataTablesConfig(object):
+    """Default configuration for Jupyter DataTables."""
+
+    classes = ['table', 'cell-border', 'nowrap']
+    """Classes to be used for the resulting DataTable."""
+
+    extensions = DataTablesExtensions
+
+    options = DataTablesOptions
+
+    limit = 1000
+    """
+    Take a sample after exceeding given limit
+    
+    :type int: limit number of rows of the table
+    """
+    sample_size = None
+
+    sort = False
+    """
+    Sort dataTable by index or values
+    
+    :type bool: enable or disable sorting by index (default: False)
+    :type str: sort by single column
+    :type List[str]: sort by multiple column(s)
+    """
+
+    warnings = True
+
+
+defaults = DataTablesConfig
