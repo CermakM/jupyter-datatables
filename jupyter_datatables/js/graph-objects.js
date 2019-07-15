693f3b65c992efb75ee27ae74d5d9f0072004c86
Marek Cermak <macermak@redhat.com>
2019-07-15 10:40:13 +0200

    Optimize width of the chart canvas
    Optimize with respect to an optimal width of a cell (970px) and 5
    columns in the DataTable.
    
    Signed-off-by: Marek Cermak <macermak@redhat.com>
    
    modified:   jupyter_datatables/js/graph-objects.js

--- jupyter_datatables/js/graph-objects.js
+++ jupyter_datatables/js/graph-objects.js
@@ -7,7 +7,7 @@ define("graph-objects", ["moment", "chartjs"], function (moment, chartjs) {
 
     const layout = Chart.defaults.global.layout
 
-    layout.width   = "150px"
+    layout.width   = "171.5px"
     layout.margin  = "auto"
     layout.padding = { left: 2, right: 2, top: 5, bottom: 5 }
 
